52bf64369d02fc9dfe929102ab98291c0835bdab
alex-the-pigeon <xelakart@gmail.com>
2023-07-24 20:04:56 +1000

    create CLI command for creating dropping and seeding users

---
+++ src/controllers/cli_controller.py
@@ -0,0 +1,36 @@
+from flask import Blueprint
+from init import db, bcrypt 
+from models.user import User 
+
+db_commands = Blueprint('db', '__name__')
+
+# this organises our routes so we don't have to type out full route every time
+# auth_routes = Blueprint('auth', __name__, url_prefix="/auth/")
+
+# @auth_routes.route("/register")
+
+# database commands to create, delete and update user table
+@db_commands.cli.command('create')
+def create_db():
+    db.create_all()
+    print("Tables Created")
+
+@db_commands.cli.command('drop')
+def drop_db():
+    db.drop_all()
+    print("Tables Dropped")
+
+@db_commands.cli.command('seed')
+def seed_db():
+    users = [
+       # creating an admin for example
+        User(
+            username="admin",
+            email="admin@admin.com",
+            password=bcrypt.generate_password_hash('admin123').decode('utf-8'),
+            is_admin=True
+        )
+    ]
+    db.session.add_all(users)
+    db.session.commit()
+    print("Tables Seeded")
--- src/models/user.py
+++ src/models/user.py
@@ -3,17 +3,17 @@ from init import db, ma
 # creating class and naming table
 class User(db.Model):
     __tablename__ = 'user'
-# table attributes
+# table attributes !!! having issues when trying to seed table. sqlalchemy.exc.DataError: (psycopg2.errors.StringDataRightTruncation) value too long for type character varying(50)
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
 # this shows the fields we want to convert to JSON for the front end
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'email', 'password', 'is_admin')
+        fields = ('id', 'username', 'email', 'password', 'is_admin')
 
 # specifying one user and many users, excluding user password from being sent to front end for security
 user_schema = UserSchema(exclude=['password'])
